d3bcb15bac1f3b804bbf03794c53d647ec890e9f
daniel kelly <danieljkelly91@gmail.com>
2018-03-09 14:24:40 +0000

    casting example

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double x = 4;
+        int y = 2;
+        int z = (int)x/y;  //this will make z an int instead of a double
+        System.out.println(z);
+    }
+//whatever you want to cast to goes in the brackets.
+
+
+
+}
